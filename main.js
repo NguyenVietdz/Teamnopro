03ada273194a91a99f95deea4f5991f5519f925b
NguyenVietdz <166784106+NguyenVietdz@users.noreply.github.com>
2024-04-12 18:12:23 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-// Ưu tiên ()
-let diemToan = 9.5;
-let diemVan = 8.75;
+// 010. Toán Tử Tăng/Giảm: Toán tử tiền tố hậu tố (++, --)
+// - Dùng để tăng hoặc giảm giá trị của biến một đơn vị.
+let a = 99;
+let b = 10;
+let c = 77;
+let d = 88;
+a++; // tương đương với a = a + 1; --> 100
+b--; // tương đương với b = b - 1; --> 9
+++c; // tương đương với c = c + 1; --> 78
+--d; // tương đương với  = a + 1; --> 87
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+// Phép tính đơn lẻ viết --, ++ trước hay sau biến đều được
 
-let dtb = (diemToan + diemVan) / 2;
-console.log(dtb);
-
-// right to left
-let x = (y = 25);
-// left to right
-let c = 25 - 7 + 8 - 1;
+// 2. Trường hợp biểu thức phức tạp
+let x = 1;
+let y = 2;
+let z = x++ - ++y + 1;
+console.log(x); // 2
+console.log(y); // 3
+console.log(z); //-1
